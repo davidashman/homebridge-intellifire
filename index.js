8706341ecc1d882fb49383ec290e90a952c06275
David Ashman <david.ashman@interfolio.com>
2023-02-14 10:26:22 -0500

    Refactor

--- index.js
+++ index.js
@@ -3,14 +3,7 @@
 let Service, Characteristic, api;
 
 const request = require("request");
-const _http_base = require("homebridge-http-base");
-const http = _http_base.http;
-const configParser = _http_base.configParser;
-const PullTimer = _http_base.PullTimer;
-const notifications = _http_base.notifications;
-const MQTTClient = _http_base.MQTTClient;
-const Cache = _http_base.Cache;
-const utils = _http_base.utils;
+const PullTimer = require("homebridge-http-base").PullTimer;
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
@@ -18,114 +11,87 @@ module.exports = function (homebridge) {
 
     api = homebridge;
 
-    homebridge.registerAccessory("homebridge-intellifire", "Fireplace", Fireplace);
+    homebridge.registerPlatform("homebridge-intellifire", "Intellifire", Intellifire);
 };
 
-function Fireplace(log, config) {
-    this.log = log;
-    this.name = config.name;
-    this.debug = config.debug || false;
+function Intellifire(log, config, api) {
+    // store restored cached accessories here
+    this.accessories = [];
+    this.fireplaces = [];
 
-    this.timeout = config.timeout;
-    if (typeof this.timeout !== 'number') {
-        this.timeout = 1000;
-    }
-
-    this.statusCache = new Cache(config.statusCache, 0);
-    if (config.statusCache && typeof config.statusCache !== "number")
-        this.log.warn("Property 'statusCache' was given in an unsupported type. Using default one!");
-    }
-
-    let jar = request.jar();
-    request.post({ url: "https://iftapi.net/a//login", jar: jar}, function(e, r, b) {
-        request.get({ url: "https://iftapi.net/a//enumlocations", jar: jar}, function(e, r, b) {
-            let data = JSON.parse(b);
-            let location_id = data.locations[0].location_id;
-            request.get({ url: `https://iftapi.net/a//enumfireplaces?location_id=${location_id}`, jar: jar}, function(e, r, b) {
+    api.on('didFinishLaunching', () => {
+        const jar = request.jar();
+        request.post({ url: "https://iftapi.net/a//login", jar: jar}, function(e, r, b) {
+            request.get({ url: "https://iftapi.net/a//enumlocations", jar: jar}, function(e, r, b) {
                 let data = JSON.parse(b);
-                this.serialNumber = data.fireplaces[0].serial;
-                request.get({ url: `https://iftapi.net/a/${this.serialNumber}//apppoll`, jar: jar}, function(e, r, b) {
-                    this.statusCache.queried(); // we only update lastQueried on successful query
+                let location_id = data.locations[0].location_id;
+                request.get({ url: `https://iftapi.net/a//enumfireplaces?location_id=${location_id}`, jar: jar}, function(e, r, b) {
                     let data = JSON.parse(b);
-                    this.firmwareVersion = data.firmware_version;
-                });
-            })
-        })
-    }).form({ username: config.auth.username, password: config.auth.password});
-    this.cookieJar = jar;
+                    data.fireplaces.forEach((f) => {
+                        const uuid = this.api.hap.uuid.generate(f.serial);
+                        if (!this.accessories.find(accessory => accessory.UUID === uuid)) {
+                            // create a new accessory
+                            const accessory = new this.api.platformAccessory(f.name, uuid);
+                            const fireplace = Fireplace(log, f.name, f.serial, '1.0', accessory, jar);
+                            this.fireplaces.push(fireplace);
+                            api.registerPlatformAccessories('homebridge-intellifire', 'Intellifire', [accessory]);
+                        }
+                    });
 
-    this.homebridgeService = new Service.Switch(this.name);
-    const onCharacteristic = this.homebridgeService.getCharacteristic(Characteristic.On)
+                })
+            })
+        }).form({ username: config.auth.username, password: config.auth.password});
+    });
+}
+
+Intellifire.prototype = {
+
+    /**
+     * REQUIRED - Homebridge will call the "configureAccessory" method once for every cached
+     * accessory restored
+     */
+    configureAccessory: function (accessory) {
+        this.accessories.push(accessory);
+    }
+
+}
+
+function Fireplace(log, name, serialNumber, firmware_version, accessory, cookieJar) {
+
+    this.log = log;
+    this.accessory = accessory;
+    this.serialNumber = serialNumber;
+    this.cookieJar = cookieJar;
+
+    const informationService = new Service.AccessoryInformation();
+    informationService
+        .setCharacteristic(Characteristic.Manufacturer, "Hearth and Home")
+        .setCharacteristic(Characteristic.Model, "Intellifire")
+        .setCharacteristic(Characteristic.SerialNumber, serialNumber)
+        .setCharacteristic(Characteristic.FirmwareRevision, firmware_version);
+    accessory.addService(informationService);
+
+    this.service = new Service.Switch(name);
+    this.service.getCharacteristic(Characteristic.On)
         .on("get", this.getStatus.bind(this))
         .on("set", this.setStatus.bind(this));
+    accessory.addService(this.service);
 
-    this.pullTimer = new PullTimer(this.log, config.pullInterval || 60, this.getStatus.bind(this), value => {
-        this.homebridgeService.getCharacteristic(Characteristic.On).updateValue(value);
+    this.pullTimer = new PullTimer(this.log, 60, this.getStatus.bind(this), value => {
+        this.service.getCharacteristic(Characteristic.On).updateValue(value);
     });
     this.pullTimer.start();
+
+    return this;
 }
 
 Fireplace.prototype = {
-
-    identify: function (callback) {
-        this.log("Identify requested!");
-        callback();
-    },
-
-    getServices: function () {
-        if (!this.homebridgeService)
-            return [];
-
-        const informationService = new Service.AccessoryInformation();
-
-        informationService
-            .setCharacteristic(Characteristic.Manufacturer, "Hearth and Home")
-            .setCharacteristic(Characteristic.Model, "Intellifire")
-            .setCharacteristic(Characteristic.SerialNumber, this.serialNumber)
-            .setCharacteristic(Characteristic.FirmwareRevision, 1);
-
-        return [informationService, this.homebridgeService];
-    },
-
-    /** @namespace body.characteristic */
-    handleNotification: function(body) {
-        const value = body.value;
-
-        let characteristic;
-        switch (body.characteristic) {
-            case "On":
-                characteristic = Characteristic.On;
-                break;
-            default:
-                this.log("Encountered unknown characteristic handling notification: " + body.characteristic);
-                return;
-        }
-
-        if (this.debug)
-            this.log("Updating '" + body.characteristic + "' to new value: " + body.value);
-
-        if (this.pullTimer)
-            this.pullTimer.resetTimer();
-
-        this.homebridgeService.getCharacteristic(characteristic).updateValue(value);
-    },
 
     getStatus: function (callback) {
         if (this.pullTimer)
             this.pullTimer.resetTimer();
 
-        if (!this.statusCache.shouldQuery()) {
-            const value = this.homebridgeService.getCharacteristic(Characteristic.On).value;
-            if (this.debug)
-                this.log(`getStatus() returning cached value '${value? "ON": "OFF"}'${this.statusCache.isInfinite()? " (infinite cache)": ""}`);
-            callback(null, value);
-            break;
-        }
-
-        if (this.debug)
-            this.log("getStatus() doing http request...");
-
-        request.get({ url: `https://iftapi.net/a/${this.serialNumber}//apppoll`, jar: jar}, function(e, r, b) {
+        request.get({ url: `https://iftapi.net/a/${this.serialNumber}//apppoll`, jar: this.cookieJar}, function(e, r, b) {
             this.statusCache.queried(); // we only update lastQueried on successful query
             let data = JSON.parse(b);
             callback(null, (data.power === "1"));
@@ -136,11 +102,7 @@ Fireplace.prototype = {
         if (this.pullTimer)
             this.pullTimer.resetTimer();
 
-        this._makeSetRequest(on, callback);
-    },
-
-    _makeSetRequest: function (on, callback) {
-        request.post({ url: `https://iftapi.net/a/${this.serialNumber}//apppost`, jar: jar}, function(e, r, b) {
+        request.post({ url: `https://iftapi.net/a/${this.serialNumber}//apppost`, jar: this.cookieJar}, function(e, r, b) {
             callback();
         }).form({power: (on ? 1 : 0)});
     },
